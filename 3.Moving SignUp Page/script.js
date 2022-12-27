42ed0eddc157dd72c1026423551f3071fa179ff5
dhanush_theijas <21cs033@kpriet.ac.in>
2022-12-27 14:52:45 +0530

    Day 3

---
+++ 3.Moving SignUp Page/script.js
@@ -0,0 +1,47 @@
+'use strict'
+
+// Input Fields
+let uname = document.getElementById('uname')
+let passwd = document.getElementById('passwd')
+
+// Button
+let flex = document.querySelector('.flex-btn')
+let btn = document.getElementById('btn')
+let str = '';
+let allow = false;
+
+let chechStr = (str) => {
+  return (str.length > 7)?true:false;
+}
+passwd.addEventListener('keydown', (e) => {
+  if(e.key != 'Tab' || e.key != 'Shift' || 
+  e.key != 'Control' || e.key != 'Meta' || 
+  e.key != 'Alt' || e.key != 'Backspace' || 
+  e.key != 'ArrowLeft' || e.key != 'ArrowRight' || e.key != 'ArrowUp' || e.key != 'ArrowDown'){
+    str = str + e.key;
+    allow = chechStr(str);
+  }
+})
+
+btn.addEventListener("mouseover", function() {
+  if(allow != false){
+    let style = window.getComputedStyle(flex);
+    flex.style.justifyContent = (style.getPropertyValue("justify-content") != "start")?'center':
+    (style.getPropertyValue("justify-content") != "center")?'end':'start';
+
+    // !This is the same as the above line but using if else for better understanding
+    /*
+    if (style.getPropertyValue("justify-content") != "start") {
+      flex.style.justifyContent = 'center';
+    }
+    else if(style.getPropertyValue("justify-content") != "center") {
+      flex.style.justifyContent = 'end';
+    }
+    else if(style.getPropertyValue("justify-content") != "end") {
+      flex.style.justifyContent = 'start';
+    }
+      */
+  }
+  else
+  flex.classList.remove("flex-btn__sec")
+});
