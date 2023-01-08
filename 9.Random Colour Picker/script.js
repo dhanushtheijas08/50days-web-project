e38a98c97bc3449f8905ee6739d5bb239dfed856
dhanush_theijas <21cs033@kpriet.ac.in>
2023-01-08 21:48:56 +0530

    Day 9

---
+++ 9.Random Colour Picker/script.js
@@ -0,0 +1,27 @@
+"use strict";
+const container = document.querySelector(".container");
+let hex = document.querySelector(".color-hex");  
+// let rgb = document.querySelector(".color-rgb");  
+// let hsl = document.querySelector(".color-hsl");  
+let randomColourPicker = function() {
+  const colors = "0123456789ABCDF".split("");
+  let color = "#";
+  for(let i=0;i<6;i++) {
+    let index = Math.floor(Math.random() * 15);
+    color = color + colors[`${index}`];
+  }
+    console.log("🚀 | file: script.js:13 | randomColourPicker | color", typeof color);
+    container.style.backgroundColor = color;
+    hex.textContent = color;
+}
+randomColourPicker();
+window.addEventListener("keydown",(e) => {
+  if(e.key == " ")
+  randomColourPicker();
+})
+
+let sample = 11;
+console.log(typeof sample);
+sample = sample.toString(16);
+console.log("🚀 | file: script.js:26 | sample", sample)
+console.log(typeof sample);
